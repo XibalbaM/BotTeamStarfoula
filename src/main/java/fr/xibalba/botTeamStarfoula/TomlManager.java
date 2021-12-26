4701c28eec11f863844afea988fc97e077ab6a35
Xibalba <xibalba2minecraft@gmail.com>
2021-12-26 21:40:00 +0100

    V1.1 - Token request

--- src/main/java/fr/xibalba/botTeamStarfoula/TomlManager.java
+++ src/main/java/fr/xibalba/botTeamStarfoula/TomlManager.java
@@ -2,13 +2,37 @@ package fr.xibalba.botTeamStarfoula;
 
 import com.moandjiezana.toml.Toml;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class TomlManager {
 
     private Toml toml;
 
     public TomlManager(File file) {
+
+        if (!file.exists()) {
+            file.getParentFile().mkdirs();
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Please enter the bot token: ");
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+
+                writer.write("token = '" + new Scanner(System.in).nextLine() + "'");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
 
         this.toml = new Toml().read(file);
     }
