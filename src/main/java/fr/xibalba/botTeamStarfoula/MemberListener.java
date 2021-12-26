2639731c99229e0041739d1fbfb2e6c0dff7dfc3
Xibalba <xibalba2minecraft@gmail.com>
2021-12-26 21:15:59 +0100

    V1.0 - Join message

--- src/main/java/fr/xibalba/botTeamStarfoula/MemberListener.java
+++ src/main/java/fr/xibalba/botTeamStarfoula/MemberListener.java
@@ -3,12 +3,19 @@ package fr.xibalba.botTeamStarfoula;
 import discord4j.core.event.domain.guild.MemberJoinEvent;
 import discord4j.core.event.domain.guild.MemberLeaveEvent;
 import discord4j.core.object.entity.Guild;
+import discord4j.core.object.entity.Member;
+
+import java.time.Instant;
 
 public class MemberListener {
 
     public static void join(MemberJoinEvent event) {
 
-        event.getGuild().flatMap(Guild::getSystemChannel).block().createEmbed(legacyEmbedCreateSpec -> legacyEmbedCreateSpec.addField("Test", "SAlut", false)).subscribe();
+        Member member = event.getMember();
+
+        event.getGuild().flatMap(Guild::getSystemChannel).block().createEmbed(legacyEmbedCreateSpec -> legacyEmbedCreateSpec.setTitle("Bienvenu").setTimestamp(
+                Instant.now()).setThumbnail(member.getAvatarUrl()).setDescription(member.getMention() + " nous à rejoint ! Nous somme maintenant " +
+                                                                              event.getGuild().map(guild -> guild.getMemberCount()).block() + " sur le serveur")).subscribe();
     }
 
     public static void leave(MemberLeaveEvent event) {
